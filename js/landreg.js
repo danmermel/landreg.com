a7fe7c59241e7936ede80f340dad6f3f90cb00c1
Daniel Mermelstein <danmermel@gmail.com>
2016-11-17 13:51:15 +0000

    maps

--- js/landreg.js
+++ js/landreg.js
@@ -200,7 +200,7 @@
              L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
                   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
                   maxZoom: 18,
-                  id: 'landreg',
+                  id: 'mapbox.streets',
                   accessToken: 'pk.eyJ1IjoibGFuZHJlZyIsImEiOiJjaXZtZTlwOWUwMDlnMnpwa2N6ejJsOXI3In0.3MtsxSmK4UcNpqxgzDj8Yw'
                   }).addTo(mymap);
           }
